7f5badba1112a32a4dc398a9bbd0a9e18fa39666
Daniel Schmidt <bigjk55@gmail.com>
2018-06-27 22:05:32 +0200

    Raw formating tool

---
+++ tools/rawfmt/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"bytes"
+	"io/ioutil"
+
+	"github.com/BigJk/dfon"
+)
+
+func main() {
+	dir := flag.String("dir", "./", "Directory to format")
+	flag.Parse()
+
+	err := filepath.Walk(*dir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if filepath.Ext(path) == ".txt" {
+			file, err := os.Open(path)
+			if err != nil {
+				return nil
+			}
+			defer file.Close()
+
+			head, err := dfon.Parse(file)
+			if err != nil {
+				return nil
+			}
+
+			var buf bytes.Buffer
+			head.Print(&buf)
+			ioutil.WriteFile(path, buf.Bytes(), 0777)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("walk error [%v]\n", err)
+	}
+}
